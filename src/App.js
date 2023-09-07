20db0df41bc8fde9ba47734eae3e98680f250101
Nishant Agarwal <nishant.agarwal@globallogic.com>
2023-09-07 23:41:05 +0530

    Deploy changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
-import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
+import { useState } from "react";
+import Alert from "./components/Alert";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    console.log(message);
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#0c4278';
+      showAlert("Dark mode enabled", 'success');
+      document.title = "Dark";
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode enabled", 'success');
+      document.title = "Light";
+    }
+  };
   return (
     <>
-      <NavBar title="MyTextUtils" about="myAbout"/>
+      {/* <Router> */}
+      <NavBar title="MyTextUtils" about="myAbout" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-
-      <TextForm heading="Enter your text"/>
-      {/* <About/> */}
+        {/* <Routes>
+            <Route exact path="/about" element = {<About />}/>
+            <Route exact path="/" element= {<TextForm showAlert={showAlert} heading="Enter your text" mode={mode} />} />
+              
+          </Routes> */}
+        <TextForm showAlert={showAlert} heading="Enter your text" mode={mode} />
       </div>
+      {/* </Router> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,10 +7,12 @@ export default function TextForm(prop) {
         let newText = text.toUpperCase();
         setText(newText);
         // console.log("btn click+ " + newText);
+        prop.showAlert("Coverted to Uppercase", "success");
     };
     const handleLowerClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        prop.showAlert("Coverted to LowerCase", "success");
     };
     const handleOnChange = (event) => {
         // console.log("onchange")
@@ -18,27 +20,29 @@ export default function TextForm(prop) {
     };
     const handleClear = ()=>{
         setText('');
+        prop.showAlert("Text Cleared!!", "success");
     };
 
-    const [text, setText] = useState("Enter text");
+    const [text, setText] = useState("");
     
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color: prop.mode==='dark'?'white':'black'}}>
             <h1>{prop.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange}
+                 style={{backgroundColor: prop.mode==='dark'?'grey':'white', color: prop.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
             </div>
             <button className='btn btn-primary mx-2' onClick={handleUpClick}> ToUpper</button>
             <button className='btn btn-primary mx-2' onClick={handleLowerClick}> ToLower</button>
             <button className='btn btn-primary mx-2' onClick={handleClear}> Clear</button>
         </div>
-        <div className="container my-4">
+        <div className="container my-4" style={{color: prop.mode==='dark'?'white':'black'}}>
             <h1> Your text summary </h1>
             <p>{text.split(" ").length} words and {text.length} characters </p>
             <p>{0.008 * text.split(" ").length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:'No Preview'}</p>
         </div>
         </>
     )
